ad31e78fc748dc53457412a02670aaa376c9961b
GeethuRadhakrishnan <geethuradhakrishnan16@gmail.com>
2022-12-09 08:00:42 +0530

    driver commit

--- src/test/java/seleniumCommands/BasicCommands.java
+++ src/test/java/seleniumCommands/BasicCommands.java
@@ -6,15 +6,16 @@ import org.openqa.selenium.edge.EdgeDriver;
 
 public class BasicCommands {
     public static void main(String[] args) {
-       // String driverPath= System.getProperty(user.dir);
-        //WebDriver driver=new ChromeDriver();
-       // driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
-       // driver.manage().window().maximize();
-        //driver.close();
-
-        WebDriver driver1 = new EdgeDriver();
-        // driver1.get("https://selenium.obsqurazone.com/simple-form-demo.php");
-         driver1.manage().window().maximize();
-        driver1.close();
+        WebDriver driver;
+        // String driverPath= System.getProperty(user.dir);
+        driver = new ChromeDriver();
+        driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
+        driver.manage().window().maximize();
+        driver.close();
+        /**Launching Edge browser**/
+        driver = new EdgeDriver();
+        driver.get("https://selenium.obsqurazone.com/simple-form-demo.php");
+        driver.manage().window().maximize();
+        driver.close();
     }
 }
